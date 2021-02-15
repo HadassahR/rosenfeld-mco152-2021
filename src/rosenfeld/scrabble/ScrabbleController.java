53f2238325b3acd8811cfbe5268501b540ee633c
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-02-15 12:32:15 -0500

    Homework changes, still a null pointer exception

--- src/rosenfeld/scrabble/ScrabbleController.java
+++ src/rosenfeld/scrabble/ScrabbleController.java
@@ -2,17 +2,20 @@ package rosenfeld.scrabble;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class ScrabbleController {
     @FXML
     TextField wordField;
+    @FXML
+    Label trueFalseLabel;
 
     public void checkDictionary(ActionEvent actionEvent) {
         try {
             String wordToCheck = wordField.getText();
             Scrabble scrabble = new Scrabble();
-            System.out.println(scrabble.checkForWord(wordToCheck));
+            trueFalseLabel.setText(String.valueOf(scrabble.checkForWord(wordToCheck)));
         } catch (Exception exc) {
             System.out.println("EXCEPTION!");
         }

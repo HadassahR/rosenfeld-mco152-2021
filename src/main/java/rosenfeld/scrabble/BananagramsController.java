e64ceac62fc34ff2852201e38ef036f4c19afe63
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-03-09 17:51:52 -0500

    Formatting changes

--- src/main/java/rosenfeld/scrabble/BananagramsController.java
+++ src/main/java/rosenfeld/scrabble/BananagramsController.java
@@ -1,8 +1,28 @@
 package rosenfeld.scrabble;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 
 public class BananagramsController {
+    public Label tile1;
+    public Label tile2;
+    public Label tile3;
+    public Label tile4;
+    public Label tile5;
+    public Label tile6;
+    public Label tile7;
+    public Label let1;
+    public Label let2;
+    public Label let3;
+    public Label let4;
+    public Label let5;
+    public Label let6;
+    public Label let7;
+    public Label points;
 
-    public Label blank1;
+    public void submitWord(ActionEvent actionEvent) {
+    }
+
+    public void clearTiles(ActionEvent actionEvent) {
+    }
 }

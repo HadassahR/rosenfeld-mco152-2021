d1c03250bc3a375afb5a215fbf60eb6f47d508b7
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-12-06 20:37:59 -0500

    Added test

--- src/test/java/rosenfeld/scrabble/webapp/DictionaryServletTest.java
+++ src/test/java/rosenfeld/scrabble/webapp/DictionaryServletTest.java
@@ -12,7 +12,7 @@ import static org.mockito.Mockito.*;
 public class DictionaryServletTest {
 
     @Test
-    public void doGet() throws IOException {
+    public void valid_doGet() throws IOException {
 
         // given
         DictionaryServlet servlet = new DictionaryServlet();
@@ -23,6 +23,25 @@ public class DictionaryServletTest {
 
         doReturn(out).when(response).getWriter();
         doReturn("PATH").when(request).getParameter("inputWord");
+
+        // when
+        servlet.doGet(request, response);
+
+        // then
+        verify(out).println(expectedDefinition);
+    }
+
+    public void invalid_doGet() throws IOException {
+
+        // given
+        DictionaryServlet servlet = new DictionaryServlet();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        PrintWriter out = mock(PrintWriter.class);
+        String expectedDefinition = "Invalid word";
+
+        doReturn(out).when(response).getWriter();
+        doReturn("LKSJFOIE").when(request).getParameter("inputWord");
 
         // when
         servlet.doGet(request, response);
